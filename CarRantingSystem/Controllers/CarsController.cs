10f77b9d843a8e260f1b126e55df0b6ba1e15151
Bogdan <bogdanpetrov2@yahoo.com>
2021-10-22 17:00:44 +0300

    cardes2

--- CarRantingSystem/Controllers/CarsController.cs
+++ CarRantingSystem/Controllers/CarsController.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-
 namespace CarRantingSystem.Controllers
 {
-    public class CarsController
+    public class CarsController : Controller
     {
+        public IActionResult Add => View();
+
+        [HttpPost]
+        public IActionResult Add()
     }
 }
