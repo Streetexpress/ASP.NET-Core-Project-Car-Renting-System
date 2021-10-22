eff7c4c48ba7e3de2f9e96020d50deabc86b2bf1
Bogdan <bogdanpetrov2@yahoo.com>
2021-10-22 12:31:50 +0300

    ddd
    test

---
+++ CarRantingSystem/Data/CatRentingDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace CarRantingSystem.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    public class CatRentingDbContext : IdentityDbContext
+    {
+        public CatRentingDbContext(DbContextOptions<CatRentingDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
